81133ddc68d380516e8d321ea3926cbe3a5d1ee9
Ayaz Ahmed Khan <ayaz@ayaz.local>
2016-12-30 13:11:04 +0500

    Adds set and get calls

--- app.py
+++ app.py
@@ -20,14 +20,24 @@ def get_faqs():
     """Return a list of FAQ titles and their links.
     """
     query = request.args.get('query', None)
-    results = 'OK'
-#    if query:
-#        results = redis_store.get(query)
-#    else:
-#        redis_store.set('timestamp', str(datetime.now()))
-#        results = 'Redis key stored'
- 
+    results = 'OK %s' % REDIS_URL
     return results
+
+@app.route('/get_key', methods=['GET'])
+def get_key():
+    query = request.args.get('query', 'timestamp')
+    results = redis_store.get(query)
+    return str(results)
+
+@app.route('/set_key', methods=['GET'])
+def set_key():
+    query = request.args.get('query', None)
+    value = request.args.get('value', None)
+    if query and value:
+        redis_store.set(query, value)
+        results = 'OK'
+    else:
+        return 'ERROR: query and value params missing'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
