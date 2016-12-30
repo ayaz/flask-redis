f6c8d67e428285e75b19c27bd2ee2eaacafbf54c
Ayaz Ahmed Khan <ayaz@ayaz.local>
2016-12-30 12:03:55 +0500

    Fixes some things.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b 0.0.0.0:5000 app:app -D --log-level=debug
+web: python app.py
--- app.py
+++ app.py
@@ -5,24 +5,29 @@ import json
 import os
 from datetime import datetime
 
-REDIS_URL = os.environ['REDIS_URL']
+#REDIS_URL = os.environ['REDIS_URL']
 
 app = Flask(__name__)
 #app.config.from_object('settings')
-redis_store = FlaskRedis(app)
+#redis_store = FlaskRedis(app)
+
+app.route('/', methods=['GET'])
+def index():
+    return 'Hi'
 
 @app.route('/hit', methods=['GET'])
 def get_faqs():
     """Return a list of FAQ titles and their links.
     """
     query = request.args.get('query', None)
-    if query:
-        results = redis_store.get(query)
-    else:
-        redis_store.set('timestamp', str(datetime.now()))
-        results = 'Redis key stored'
+    results = 'OK'
+#    if query:
+#        results = redis_store.get(query)
+#    else:
+#        redis_store.set('timestamp', str(datetime.now()))
+#        results = 'Redis key stored'
  
     return results
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
